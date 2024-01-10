ea6eea76f3760514c24d6d26f125bb9d605c33f8
Max <Maxandre.neveux@gmail.com>
2024-01-10 15:30:01 +0100

    Initial Commit

---
+++ Sentiment_IA_app/Sentiment_IA_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sentiment_IA_appApp.swift
+//  Sentiment_IA_app
+//
+//  Created by Maxandre Neveux on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sentiment_IA_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sentiment_IA_appUITests/Sentiment_IA_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sentiment_IA_appUITestsLaunchTests.swift
+//  Sentiment_IA_appUITests
+//
+//  Created by Maxandre Neveux on 10/01/2024.
+//
+
+import XCTest
+
+final class Sentiment_IA_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
